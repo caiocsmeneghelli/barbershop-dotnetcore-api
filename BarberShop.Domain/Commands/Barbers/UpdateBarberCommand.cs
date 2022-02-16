f83c9aebcc4d06ebf169dcfcfd72eff6f332b7db
Caio Cesar <caiocsmeneghelli@outlook.com>
2022-02-15 21:36:20 -0300

    Mudancas pequenas

---
+++ BarberShop.Domain/Commands/Barbers/UpdateBarberCommand.cs
@@ -0,0 +1,30 @@
+using BarberShop.Domain.Commands.Contracts;
+using Flunt.Notifications;
+using Flunt.Validations;
+
+namespace BarberShop.Domain.Commands{
+    public class UpdateBarberCommand : Notifiable, ICommand
+    {
+        public UpdateBarberCommand()
+        {
+        }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+
+        public void Validate()
+        {
+            AddNotifications(
+                new Contract()
+                    .Requires()
+                    .HasMinLen(Email, 6, "Email", "The value Email, must have more than 6 char.")
+                    .HasMaxLen(Email, 64, "Email", "The value Email, must have less than 64 char.")
+                    .HasMinLen(FirstName, 2, "FirstName", "The value FirstName, must have at least 2 char.")
+                    .HasMaxLen(FirstName, 64, "Firstname", "The value FirstName, must have less than 64 char.")
+                    .HasMinLen(LastName, 2, "LastName", "The value LastName, must have at least 2 char.")
+                    .HasMaxLen(LastName, 64, "LastName", "The value LastName, must have less than 64 char.")
+            );
+        }
+    }
+}
