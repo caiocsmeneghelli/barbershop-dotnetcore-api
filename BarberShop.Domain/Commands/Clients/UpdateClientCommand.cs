8ab9718960fc1c970041785f035a40ed9b7c296d
Caio Cesar <caiocsmeneghelli@outlook.com>
2022-03-02 15:40:50 -0300

    Alterando testes de criacao de Client e Update.

---
+++ BarberShop.Domain/Commands/Clients/UpdateClientCommand.cs
@@ -0,0 +1,38 @@
+using System;
+using BarberShop.Domain.Commands.Contracts;
+using Flunt.Notifications;
+using Flunt.Validations;
+
+namespace BarberShop.Domain.Commands
+{
+    public class UpdateClientCommand : Notifiable, ICommand
+    {
+        public UpdateClientCommand()
+        { }
+        public UpdateClientCommand(Guid id, string firstName, string lastName, string number)
+        {
+            this.Id = id;
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Number = number;
+
+        }
+        public Guid Id {get;set;}
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Number { get; set; }
+
+        public void Validate()
+        {
+            AddNotifications(
+                new Contract()
+                .Requires()
+                .HasMinLen(FirstName, 2, "FirstName", "The value FirstName, must have at least 2 char.")
+                .HasMaxLen(FirstName, 64, "Firstname", "The value FirstName, must have less than 64 char.")
+                .HasMinLen(LastName, 2, "LastName", "The value LastName, must have at least 2 char.")
+                .HasMaxLen(LastName, 64, "LastName", "The value LastName, must have less than 64 char.")
+                .HasExactLengthIfNotNullOrEmpty(Number, 11, "PhoneNumber", "Phone number must contain 11 char.")
+            );
+        }
+    }
+}
---
+++ BarberShop.Tests/CommandTests/Clients/ClientCommandTests.cs
@@ -0,0 +1,42 @@
+using BarberShop.Domain.Commands;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BarberShop.Tests.CommandTests.Clients{
+    [TestClass]
+    public class ClientCommandTests{
+        private readonly CreateClientCommand _createValidCommand = new CreateClientCommand("Caio Cesar", "Serrano Meneghelli", "27999096912");
+        private readonly CreateClientCommand _createInvalidCommand = new CreateClientCommand("Caio Cesar", "Serrano Meneghelli", "2799909691222");
+
+        private readonly UpdateClientCommand _updateValidCommand = new UpdateClientCommand(System.Guid.NewGuid(), "Caio Cesar", "Serrano Meneghelli", "27999096912");
+        private readonly UpdateClientCommand _updateInvalidCommand = new UpdateClientCommand(System.Guid.Empty, "Caio Cesar", "Serrano Meneghelli", "2799909691222");
+
+        public ClientCommandTests()
+        {
+            _createValidCommand.Validate();
+            _createInvalidCommand.Validate();
+
+            _updateValidCommand.Validate();
+            _updateInvalidCommand.Validate();
+        }
+
+        [TestMethod]
+        public void Dado_um_Command_valido_para_criacao_de_novo_client(){
+            Assert.AreEqual(_createValidCommand.Valid, true);
+        }
+
+        [TestMethod]
+        public void Dado_um_Command_invalido_para_criacao_de_um_novo_(){
+            Assert.AreEqual(_createInvalidCommand.Valid, false);
+        }
+
+        [TestMethod]
+        public void Dado_um_command_valido_para_atualizacao(){
+            Assert.AreEqual(_updateValidCommand.Valid, true);
+        }
+
+        [TestMethod]
+        public void Dado_um_command_invalido_para_atualizacao(){
+            Assert.AreEqual(_updateInvalidCommand.Valid, false);
+        }
+    }
+}
